924a098a7099ba00a508faca80238e8092bd2a03
trungpv <phamvantrung.11193@gmail.com>
2016-07-19 23:56:34 +0700

    init

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var elixir = require('laravel-elixir');
+
+elixir.config.assetsDir = './';
+
+var bower_path = "./bower_components";
+
+var paths = {
+  'bootstrap'  : bower_path + "/bootstrap/",
+  'jquery': bower_path + '/jquery/',
+  'fontawesome': bower_path + '/font-awesome/',
+	'flatui' : bower_path + '/flat-ui/'
+};
+	
+elixir(function(mix) {
+  
+  mix.less('app.less')
+        .copy(paths.bootstrap + 'fonts/**', 'public/fonts/bootstrap')
+        .copy(paths.fontawesome + 'fonts/**', 'public/fonts/fontawesome')
+				.copy(paths.flatui + 'fonts/**', 'public/fonts/flatui')
+        .scripts([
+            paths.jquery + "jquery.js",
+						paths.flatui + "dist/js/flat-ui.js",
+            './resources/assets/js/*.js',
+        ], 'public/js/app.js', './')
+        .version([
+            'css/app.css',
+            'js/app.js'
+        ])
+
+});
---
+++ resources/assets/js/app.js
@@ -0,0 +1,5 @@
+$(function () {
+  // Checkboxes and Radio buttons
+  $('[data-toggle="checkbox"]').radiocheck();
+  $('[data-toggle="radio"]').radiocheck();
+});
